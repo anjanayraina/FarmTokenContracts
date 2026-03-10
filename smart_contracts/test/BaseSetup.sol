b5e8897b0f9dc40c461469bba74444c7a7ac70b5
Anjanay <anjanayraina326@gmail.com>
2026-03-10 10:58:25 +0530

    feat: Implement PrivateNFTVault for owner-only soft staking of ERC721s to earn PortfolioYieldToken rewards, including the new ERC20 token and a base test setup.

--- smart_contracts/test/BaseSetup.sol
+++ smart_contracts/test/BaseSetup.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
-import "../contracts/PortfolioYieldToken.sol";
-import "../contracts/PrivateNFTVault.sol";
+import "../src/PortfolioYieldToken.sol";
+import "../src/PrivateNFTVault.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 // Mock NFT collection for testing batch operations
